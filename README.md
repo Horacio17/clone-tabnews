f19d743ef6d95d65a0a6048d71fe464defb55318
Horacio17 <horacio.paganini@hotmail.com>
2024-12-14 19:15:54 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de teste, curso felipedeschamps curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
-function Home () {
-    return <h1>The One Piece is real !!!</h1>
+function Home() {
+  return <h1>The One Piece is real !!!</h1>;
 }
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
+
 export default Home;
